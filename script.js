ac330746de40d57daa94c0b917f43fc6f407b797
Silvaa88 <wsjbarber88@gmail.com>
2024-07-12 14:07:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function gerarNumero(){
+
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+    const result = Math.floor(Math.random() * (max- min + 1 ) + min )
+   document.getElementById("resultado").innerText = result
+    
+}
